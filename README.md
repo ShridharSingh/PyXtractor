28195700f41d9660d39179db6d07d262b0202463
Shridhar Singh <shridharsingh0@gmail.com>
2023-10-06 01:48:13 +0200

    final code - export to json format by default

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PyXtractor
-Python Program to extract keywords from text and pdf files
+
+PyXtractor is intended to make NLP processing easy - Allowing for quick and easy referral of what a pdf file is about. 
+
+
+PyXtractor makes use of Rake-nltk library and will extract the keywords from any pdf file specified by the user. It will also extract keywords in a user's prompt(sentence) (to be used for similarity comparison). 
+
+Keywords will then be exported into a JSON file for easy processing by other programs.
--- pyxtractor.py
+++ pyxtractor.py
@@ -11,11 +11,10 @@ def extract_text_from_pdf(pdf_file: str) -> [str]:
         for page in reader.pages:
             content = page.extract_text()
             text.write(content)
-        text.close()
+        text.close()  
 
-    rake()  
-   
-def rake():
+#Rake funtions extract keywords from text and user prompt
+def rake_text_from_pdf():
     text = open("text.txt", "r")
     input_text = text.read()
 
@@ -57,10 +56,50 @@ def rake():
     for k,v in top10:
         top10_most_occured_words.append(v)
     
-    #print or export to json : print(top10_most_occured_words)
-    export_to_json(top10_most_occured_words)
+    return top10_most_occured_words
 
-def export_to_json(top10_input_array):
+def rake_user_prompt(user_prompt):
+    rake = Rake(punctuations=['.','`','/','\'','~','-','=','"',',',':',';','+','.,',',.',
+                           '(',')','[',']','{','}',').','),','].','],','}.','}.','~/','/~',
+                           '1','2','3','4','5','6','7','8','9','0',
+                           'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+    rake.extract_keywords_from_text(user_prompt)
+    ranked = rake.get_word_frequency_distribution()
+
+    #Sorting Arrays
+    most_occured_numbers = []
+    most_occured_words = []
+    key_value = []
+    top_keywords = []
+    ordered_most_occured_words = []
+    
+    #Iterable Variables
+    iterable = 0
+
+    prompt_keywords = []
+    for k,v in ranked.items():
+        if v > 0:
+            most_occured_numbers.append(v)
+            most_occured_words.append(k)
+            key_value.append([v,k])
+        key_value.sort()
+        key_value.reverse()
+    
+    for k,v in key_value:
+        while iterable < len(key_value):
+            top_keywords.append(key_value.pop(0))
+
+    for k,v in top_keywords:
+        ordered_most_occured_words.append(v)
+
+    for k,v in ranked.items():
+        prompt_keywords.append(k)
+
+    return ordered_most_occured_words
+
+#JSON Export funtions
+#Exports keywords individually from PDF
+def export_pdf_keywords_to_json(top10_input_array):
     
     dictonary = {
         "keyword0":top10_input_array[0],
@@ -78,6 +117,34 @@ def export_to_json(top10_input_array):
     with open("input.json", "w") as outfile:
         outfile.write(json_object)
 
+#Exports keywords as array for user promp and PDF
+def export_all_to_json(pdf_keywords, prompt_keywords):
+    
+    userkeyw = []
+    pdfkeyw = []
+    
+    for word in prompt_keywords:
+        userkeyw.append(word)
+
+    for words in pdf_keywords:
+        pdfkeyw.append(words)
+
+
+    dictonary = {
+        "user_keywords":userkeyw,
+        "PDF_keywords": pdfkeyw
+    }
+    json_object = json.dumps(dictonary, indent=10)
+    with open("input.json", "w") as outfile:
+        outfile.write(json_object)
+
 if __name__ == '__main__':
     pdfname = input("Enter name of pdf you want to extract keywords from: ")
-    extracted_text = extract_text_from_pdf(pdfname)
+    userprompt = input("Enter prompt: ")
+
+    extract_text = extract_text_from_pdf(pdfname)
+
+    pdf_keywords = rake_text_from_pdf()
+    prompt_keywords = rake_user_prompt(userprompt)
+
+    export_all_to_json(pdf_keywords=pdf_keywords, prompt_keywords=prompt_keywords)
